3b360088714f5d160a9b2060cb452e5f69a2743f
RyanC <ts-cuong.chau@rakuten.com>
2020-11-03 21:14:47 +0900

    Initial Commit

---
+++ SwiftAppNoStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftAppNoStoryboard
+//
+//  Created by Chau, Cuong | DCMS on 2020/11/03.
+//  Copyright © 2020 Chau, Cuong | DCMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
